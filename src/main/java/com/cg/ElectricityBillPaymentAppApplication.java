948312b6a6c87cb05f57452d8539e7087b2496e3
lenovo <lenovo@DESKTOP-EV4CPJH>
2022-04-15 22:34:43 +0530

    hiii

--- src/main/java/com/cg/ElectricityBillPaymentAppApplication.java
+++ src/main/java/com/cg/ElectricityBillPaymentAppApplication.java
@@ -9,10 +9,8 @@ public class ElectricityBillPaymentAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ElectricityBillPaymentAppApplication.class, args);
 
-		System.out.println("this is me friend");
-    System.out.println("hi project");
-		System.out.println("Hi its working for me ");
-		System.out.println("merging?");
+		System.out.println("this is me dude");
+   
 
 
 	}
