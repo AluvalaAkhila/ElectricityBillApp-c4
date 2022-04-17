227c2428fd230d204113a060a3a8fd2cc594f0c0
lenovo <lenovo@DESKTOP-EV4CPJH>
2022-04-17 12:51:56 +0530

    POJOCLASSES

--- src/main/java/com/modules/Connection.java
+++ src/main/java/com/modules/Connection.java
@@ -1,5 +1,44 @@
 package com.modules;
 
+import java.time.LocalDate;
+
 public class Connection {
+	private long connectionId;
+	private long consumerNumber;
+	private LocalDate applicationDate;
+	private LocalDate connectionDate;
+	private  boolean connectionStatus;
+	public Connection() {
+	}
+	public long getConnectionId() {
+		return connectionId;
+	}
+	public void setConnectionId(long connectionId) {
+		this.connectionId = connectionId;
+	}
+	public long getConsumerNumber() {
+		return consumerNumber;
+	}
+	public void setConsumerNumber(long consumerNumber) {
+		this.consumerNumber = consumerNumber;
+	}
+	public LocalDate getApplicationDate() {
+		return applicationDate;
+	}
+	public void setApplicationDate(LocalDate applicationDate) {
+		this.applicationDate = applicationDate;
+	}
+	public LocalDate getConnectionDate() {
+		return connectionDate;
+	}
+	public void setConnectionDate(LocalDate connectionDate) {
+		this.connectionDate = connectionDate;
+	}
+	public boolean isConnectionStatus() {
+		return connectionStatus;
+	}
+	public void setConnectionStatus(boolean connectionStatus) {
+		this.connectionStatus = connectionStatus;
+	}
 
 }
