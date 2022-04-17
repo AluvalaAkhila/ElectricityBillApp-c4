1c73ea70e54e0173c26a913734c7d7da93619d71
lenovo <lenovo@DESKTOP-EV4CPJH>
2022-04-17 12:12:05 +0530

    added class

--- src/main/java/com/modules/Customer.java
+++ src/main/java/com/modules/Customer.java
@@ -1,14 +1,71 @@
 package com.modules;
 
-public class Customer {
-	long customerid;
-	long aadharNumber;
-	String firstName;
-	String middleName;
-	String lastName;
-	String userName;
-	String email;
-	long mobileNumber;
-	String gender;
+public class Customer extends User {
+	private long customerid;
+	private long aadharNumber;
+	private	String firstName;
+	private String middleName;
+	private String lastName;
+	private String userName;
+	private String email;
+	private long mobileNumber;
+	private String gender;
 	
+	
+	public Customer() {
+	}
+	public long getCustomerid() {
+		return customerid;
+	}
+	public void setCustomerid(long customerid) {
+		this.customerid = customerid;
+	}
+	public long getAadharNumber() {
+		return aadharNumber;
+	}
+	public void setAadharNumber(long aadharNumber) {
+		this.aadharNumber = aadharNumber;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}		
 }
