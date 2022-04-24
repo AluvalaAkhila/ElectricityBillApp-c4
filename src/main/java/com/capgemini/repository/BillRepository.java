d076ab1d8a98961506889d5f24b5110e85199d80
pamulas <pamulas@DIVYA-PAMULA>
2022-04-24 21:59:14 +0530

    fromdivya

--- src/main/java/com/capgemini/repository/BillRepository.java
+++ src/main/java/com/capgemini/repository/BillRepository.java
@@ -1,11 +1,20 @@
 package com.capgemini.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.capgemini.modules.Bill;
+import com.capgemini.modules.Customer;
 @Repository
 
 public interface BillRepository extends JpaRepository<Bill , Long>{
+	
+	
+	@Query(value="select e from Bill e where e.firstName=?1")
+	   public List<Bill> getAllCustomerByName3(@Param("firstName")String Name);
 
 }
--- src/main/java/com/repositories/BillDao.java
+++ src/main/java/com/repositories/BillDao.java
@@ -1,5 +1,15 @@
 package com.repositories;
+import java.time.LocalDate;
+import java.util.List;
+import com.capgemini.model.Bill;
 
-public interface BillDao {
+
+
+public interface BillDao 
+{
+	public Bill readBillByConsumerNumber(Long consumerNumber);
+	public Bill readBillByMobileNumber(String mobile);
+	public Bill readBillByEmail(String email);
+	public List<Bill> readBillForDateRange(LocalDate from, LocalDate to);
 
 }
